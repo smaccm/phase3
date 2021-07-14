58526e61e87435815e5e860f2e1481a815cc2dec
Michal Podhradsky <mpodhradsky@galois.com>
2021-07-14 14:57:06 -0700

    Switch camkes manifest

--- scripts/code-setup.sh
+++ scripts/code-setup.sh
@@ -50,6 +50,6 @@ cd $BASE_DIR
 mkdir camkes
 cd camkes
 export GIT_SSL_NO_VERIFY=1
-repo init -u https://github.com/smaccm/phase3.git -m phase3.xml -b 2021-update
+repo init -u https://github.com/smaccm/phase3.git -m phase3.xml -b code-upgrade
 repo sync
 repo sync -d
